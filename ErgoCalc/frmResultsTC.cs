3ae021124153583008f65341a052cbabd0fc38a5
arthurits <simply_alfy@hotmail.com>
2021-03-16 17:36:29 +0100

    Code updated

--- ErgoCalc/frmResultsTC.cs
+++ ErgoCalc/frmResultsTC.cs
@@ -87,6 +87,50 @@ namespace ErgoCalc
             }
         }
 
+        private void SerializeToJSON(Utf8JsonWriter writer)
+        {
+            writer.WriteStartObject();
+            writer.WriteString("Document type", "Thermal comfort model");
+            
+            writer.WritePropertyName("Cases");
+            writer.WriteStartArray();
+
+            foreach (var data in _data)
+            {
+                writer.WriteStartObject();
+
+                writer.WritePropertyName("Data");
+                writer.WriteStartObject();
+                writer.WriteNumber("Air temperature", data.data.TempAir);
+                writer.WriteNumber("Radiant temperature", data.data.TempRad);
+                writer.WriteNumber("Air velocity", data.data.Velocity);
+                writer.WriteNumber("Relative humidity", data.data.RelHumidity);
+                writer.WriteNumber("Clothing", data.data.Clothing);
+                writer.WriteNumber("Metabolic rate", data.data.MetRate);
+                writer.WriteNumber("External work", data.data.ExternalWork);
+                writer.WriteEndObject();
+
+                writer.WritePropertyName("Results");
+                writer.WriteStartObject();
+                writer.WriteNumber("PMV", data.factors.PMV);
+                writer.WriteNumber("PPD", data.factors.PPD);
+                writer.WriteNumber("Heat loss - Skin", data.factors.HL_Skin);
+                writer.WriteNumber("Heat loss - Sweating", data.factors.HL_Sweating);
+                writer.WriteNumber("Heat loss - Latent", data.factors.HL_Latent);
+                writer.WriteNumber("Heat loss - Dry", data.factors.HL_Dry);
+                writer.WriteNumber("Heat loss - Radiation", data.factors.HL_Radiation);
+                writer.WriteNumber("Heat loss - Convection", data.factors.HL_Convection);
+                writer.WriteEndObject();
+
+                writer.WriteEndObject();
+            }
+
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+
+            writer.Flush();
+        }
+
         #region IChildResults interface
         public ToolStrip ChildToolStrip { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
@@ -112,7 +156,49 @@ namespace ErgoCalc
 
         public bool OpenFile(JsonDocument document)
         {
-            throw new NotImplementedException();
+            bool result = true;
+            int Length;
+            ModelTC data = new ModelTC();
+            _data = new List<ModelTC>();
+            JsonElement root = document.RootElement;
+
+            try
+            {
+                foreach (JsonElement curve in root.GetProperty("Cases").EnumerateArray())
+                {
+                    data.data.TempAir = curve.GetProperty("Data").GetProperty("Air temperature").GetDouble();
+                    data.data.TempRad = curve.GetProperty("Data").GetProperty("Radiant temperature").GetDouble();
+                    data.data.Velocity = curve.GetProperty("Data").GetProperty("Air velocity").GetDouble();
+                    data.data.RelHumidity = curve.GetProperty("Data").GetProperty("Relative humidity").GetDouble();
+                    data.data.Clothing = curve.GetProperty("Data").GetProperty("Clothing").GetDouble();
+                    data.data.MetRate = curve.GetProperty("Data").GetProperty("Metabolic rate").GetDouble();
+                    data.data.ExternalWork = curve.GetProperty("Data").GetProperty("External work").GetDouble();
+
+                    data.factors.PMV = curve.GetProperty("Results").GetProperty("PMV").GetDouble();
+                    data.factors.PPD = curve.GetProperty("Results").GetProperty("PPD").GetDouble();
+                    data.factors.HL_Skin = curve.GetProperty("Results").GetProperty("Heat loss - Skin").GetDouble();
+                    data.factors.HL_Sweating = curve.GetProperty("Results").GetProperty("Heat loss - Sweating").GetDouble();
+                    data.factors.HL_Latent = curve.GetProperty("Results").GetProperty("Heat loss - Latent").GetDouble();
+                    data.factors.HL_Dry = curve.GetProperty("Results").GetProperty("Heat loss - Dry").GetDouble();
+                    data.factors.HL_Radiation = curve.GetProperty("Results").GetProperty("Heat loss - Radiation").GetDouble();
+                    data.factors.HL_Convection = curve.GetProperty("Results").GetProperty("Heat loss - Convection").GetDouble();
+
+                    _data.Add(data);
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            if (result)
+            {
+                //CalcularCurva();
+                //PlotCurves();
+                //_chartOptions.NúmeroCurva = chart.plt.GetPlottables().Count - 1;
+            }
+
+            return result;
         }
 
         public bool PanelCollapsed()
@@ -122,7 +208,48 @@ namespace ErgoCalc
 
         public void Save(string path)
         {
-            throw new NotImplementedException();
+            // Displays a SaveFileDialog so the user can save the Image  
+            SaveFileDialog SaveDlg = new SaveFileDialog
+            {
+                DefaultExt = "*.csv",
+                Filter = "ERGO file (*.ergo)|*.ergo|CSV file (*.csv)|*.csv|Text file (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 1,
+                Title = "Save scatter-plot data",
+                OverwritePrompt = true,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+
+            DialogResult result;
+            using (new CenterWinDialog(this))
+            {
+                result = SaveDlg.ShowDialog(this.Parent);
+            }
+
+            // If the file name is not an empty string open it for saving.  
+            if (result == DialogResult.OK && SaveDlg.FileName != "")
+            {
+                switch (SaveDlg.FilterIndex)
+                {
+                    case 1:
+                        var fs = SaveDlg.OpenFile();
+                        if (fs != null)
+                        {
+                            using var writer = new Utf8JsonWriter(fs, options: new JsonWriterOptions { Indented = true });
+                            SerializeToJSON(writer);
+                            //var jsonString = JsonSerializer.Serialize(_datos[0]._points[0], new JsonSerializerOptions { WriteIndented = true });
+                        }
+                        break;
+                    case 2:
+                        
+                        break;
+                }
+                using (new CenterWinDialog(this.MdiParent))
+                {
+                    MessageBox.Show(this, "The file was successfully saved", "File saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            return;
         }
 
         public void ShowHideSettings()
