87dbcb3a0dfdea65c7823e270c7528ef3e842d7b
arthurits <simply_alfy@hotmail.com>
2023-05-21 11:07:51 +0200

    Update copyright year

--- ErgoCalc/Properties/AssemblyInfo.cs
+++ ErgoCalc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Arthurits Ltd.")]
 [assembly: AssemblyProduct("ErgoCalc")]
-[assembly: AssemblyCopyright("Copyright © 2009-2022")]
+[assembly: AssemblyCopyright("Copyright © 2009-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
