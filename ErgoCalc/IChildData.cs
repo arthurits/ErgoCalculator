a3c0442789aa0d93090ab71c49b064ca59ba551a
—ins— <insht_cnct@hotmail.es>
2024-01-19 12:22:45 +0100

    Refactor code.

---
+++ ErgoCalc/IChildData.cs
@@ -0,0 +1,119 @@
+﻿namespace ErgoCalc;
+
+/// <summary>
+/// Public interface for data windows
+/// </summary>
+public interface IChildData
+{
+    public object GetData { get; }
+
+    /// <summary>
+    /// Function to load data examples
+    /// </summary>
+    //public void LoadExample();
+
+    /// <summary>
+    /// Updates the number of columns in a DataGridView control
+    /// </summary>
+    /// <param name="control">Reference to a <see cref="DataGridView"/> control</param>
+    /// <param name="value">Number of columns in the <see cref="DataGridView"/> control</param>
+    public void UpdateGridColumns(object control, int value)
+    {
+        if (control is DataGridView gridVariables)
+        {
+            // Add or remove columns
+            if (value > gridVariables.ColumnCount)
+            {
+                for (int i = gridVariables.ColumnCount; i < value; i++)
+                    AddColumn(i);
+            }
+            else if (value < gridVariables.ColumnCount)
+            {
+                for (int i = gridVariables.ColumnCount - 1; i >= value; i--)
+                    gridVariables.Columns.RemoveAt(i);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Adds a column to a DataGridView control. This function should be implemented in the derived classes
+    /// </summary>
+    /// <param name="col">Column index (0-based)</param>
+    protected internal void AddColumn(int col)
+    {
+        return;
+    }
+
+    /// <summary>
+    /// Adds a column to a DataGridView control
+    /// </summary>
+    /// <param name="control">Reference to a <see cref="DataGridView"/> control</param>
+    /// <param name="col">Column index (0-based)</param>
+    /// <param name="strName">Header text to be shown in the column</param>
+    /// <param name="width">Width, in pixels, of the column. Default value is 75</param>
+    public void AddColumnBasic(object control, int col, string strName, int width = 75)
+    {
+        if (control is not DataGridView gridVariables) return;
+
+        // By default, the DataGrid always contains a single column
+        //if (col == 0) return;
+        if (gridVariables.Columns.Contains($"Column {(col).ToString()}")) return;
+
+        // Create the new column
+        //gridVariables.Columns.Add("Column" + (col + 1).ToString(), "Task " + strTasks[col]);
+        gridVariables.Columns.Add($"Column {(col).ToString()}", $"{strName} {((char)('A' + col)).ToString()}");
+        gridVariables.Columns[col].SortMode = DataGridViewColumnSortMode.NotSortable;
+        gridVariables.Columns[col].Width = width;
+        gridVariables.Columns[col].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+        return;
+    }
+
+    /// <summary>
+    /// Adds the text-based row headers to a DataGridView control.
+    /// </summary>
+    /// <param name="control">Reference to a <see cref="DataGridView"/> control.</param>
+    /// <param name="strHeaders"><see cref="Array"/> of strings containing the row header texts</param>
+    public void AddGridRowHeaders(object control, string[] strHeaders)
+    {
+        if (control is not DataGridView gridVariables)
+            return;
+        if (strHeaders.Length == 0)
+            return;
+
+        // Create the header rows
+        gridVariables.RowCount = strHeaders.Length;
+        for (int i = 0; i < strHeaders.Length; i++)
+        {
+            gridVariables.Rows[i].HeaderCell.Value = strHeaders[i];
+        }
+
+        // Adjust the column width
+        gridVariables.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+    }
+
+    /// <summary>
+    /// Updates the number of groups in a ListView control
+    /// </summary>
+    /// <param name="control">Reference to a <see cref="ListView"/> control.</param>
+    /// <param name="value">Number of groups in the <see cref="ListView"/> control.</param>
+    /// <param name="strGroup">Group name to be shown on the ListView</param>
+    public void UpdateListView(object control, int value, string strGroup = "Task")
+    {
+        //Int32 tasks = Convert.ToInt32(updTasks.Value);
+
+        if (control is ListViewEx listViewEx)
+        {
+            if (value > listViewEx.Groups.Count)
+            {
+                for (int i = listViewEx.Groups.Count; i < value; i++)
+                    listViewEx.AddGroup(i, strGroup);
+            }
+            else if (value < listViewEx.Groups.Count)
+            {
+                for (int i = value; i < listViewEx.Groups.Count; i++)
+                    listViewEx.RemoveGroup(listViewEx.Groups.Count - 1);
+            }
+        }
+    }
+
+}
---
+++ ErgoCalc/IChildResults.cs
@@ -0,0 +1,110 @@
+﻿namespace ErgoCalc;
+
+/// <summary>
+/// Public interface for child windows used to show the results
+/// </summary>
+public interface IChildResults
+{
+    ModelType? Model { get; set; }
+
+    /// <summary>
+    /// Gets and set the child's ToolStrip control in order to be merged with the parent's
+    /// </summary>
+    ToolStrip? ChildToolStrip { get; set; }
+
+    /// <summary>
+    /// Defines the enabled state for each control in frmMain's ToolBar
+    /// </summary>
+    /// <returns>An array with 'enabled' boolean values</returns>
+    bool[] GetToolbarEnabledState();
+
+    /// <summary>
+    /// Saves the data shown in the child window into a file
+    /// </summary>
+    /// <param name="path">Path where the data should be saved</param>
+    void Save(string path);
+
+    /// <summary>
+    /// Opens a document
+    /// </summary>
+    /// <param name="document">Document</param>
+    /// <returns></returns>
+    bool OpenFile(System.Text.Json.JsonDocument document);
+    
+    /// <summary>
+    /// Updates the text output to the active culture
+    /// </summary>
+    void UpdateLanguage(System.Globalization.CultureInfo culture);
+
+    /// <summary>
+    /// Formats the text shown in the RichtTextBox
+    /// </summary>
+    void FormatText();
+
+    /// <summary>
+    /// Edits the original data of the child window
+    /// </summary>
+    void EditData();
+
+    /// <summary>
+    /// Duplicates the current child window
+    /// </summary>
+    void Duplicate();
+
+    /// <summary>
+    /// Computes the tabs positions in pixels.
+    /// </summary>
+    /// <param name="g">Graphics object</param>
+    /// <param name="font">Font used to write the text and to compute the text space</param>
+    /// <param name="numberTabs">Number of tabs to be computed</param>
+    /// <param name="rowHeaders">The first-column texts used to computed the first tab position</param>
+    /// <param name="columnHeaders">All the other column header texts used to compute the rest of tabs</param>
+    /// <param name="tabFactor">Factor (percentage) of the maximum measure to be used as tab space</param>
+    /// <param name="tabMinSpace">Minimum tab space in pixels. Default value is 10</param>
+    /// <returns>Array of tab positions in pixels</returns>
+    public int[] ComputeTabs(Graphics g, Font font, int numberTabs, string[] rowHeaders, string[] columnHeaders, double tabFactor = 0.1, int tabMinSpace = 10)
+    {
+        (int rowMax, int rowTab) = ComputeTabSpace(g, font, rowHeaders, tabFactor, tabMinSpace);
+        (int colMax, int colTab) = ComputeTabSpace(g, font, columnHeaders, tabFactor, tabMinSpace);
+        int tab = Math.Min(rowTab, colTab);
+
+        int[] tabs = new int[numberTabs];
+        for (int i = 0; i < numberTabs; i++)
+        {
+            if (i == 0)
+                tabs[i] = rowMax + tab;
+            else
+                tabs[i] = tabs[i - 1] + colMax + tab;
+        }
+
+        return tabs;
+    }
+
+    /// <summary>
+    /// Computes the tabs using a particular <see cref="Graphics"/> object and <see cref="Font"/>
+    /// </summary>
+    /// <param name="strings">Array of strings that will be measured. The greatest measure is used to compute the tab space</param>
+    /// <param name="tabFactor">Factor (percentage) of the maximum measure to be used as tab space</param>
+    /// <param name="tabMinSpace">Minimum tab space in pixels. Default value is 10</param>
+    /// <returns>The maximum width of the string array in pixels and the corresponding tab space to be added to the width</returns>
+    public (int maxWidth, int tabSpace) ComputeTabSpace(Graphics g, Font font, string[] strings, double tabFactor = 0.1, int tabMinSpace = 10)
+    {
+        SizeF size;
+        int nWidth = 0;
+        int tabSpace;
+
+        //using var g = rtbShowResult.CreateGraphics();
+        foreach (string strRow in strings)
+        {
+            size = g.MeasureString(strRow, font);
+            if (size.Width > nWidth)
+                nWidth = (int)size.Width;
+        }
+
+        tabSpace = (int)(nWidth * tabFactor);
+        tabSpace = tabSpace > tabMinSpace ? tabSpace : tabMinSpace;
+
+        return (nWidth, tabSpace);
+    }
+
+}
