f73ac65881337add6737baa811d2f1b605adac02
arthurits <simply_alfy@hotmail.com>
2024-02-09 11:22:59 +0100

    Add unit test project

---
+++ ErgoCalc.UnitTest/LibertyMutualTest.cs
@@ -0,0 +1,15 @@
+using ErgoCalc;
+using ErgoCalc.Models.LibertyMutual;
+
+namespace ErgoCalc.UnitTest;
+
+[TestClass]
+public class LibertyMutualTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+       
+
+    }
+}
